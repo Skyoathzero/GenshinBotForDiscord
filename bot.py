8f3b0271a50498ebde74deab90e4c5e10c72fdec
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2020-12-25 11:22:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,16 +8,18 @@ cwd = Path(__file__).parents[0]
 cwd = str(cwd)
 print(f"{cwd}\n----")
 
-secret_file = json.load(open(cwd+"/bot_config/token.json"))
+secret_file = json.load(open(cwd+"/token.json"))
 bot = commands.Bot(command_prefix="g!")
 logging.basicConfig(level=logging.INFO)
 
 @bot.event
 async def on_ready():
-    print(Logged in)
+    print("logged in")
     await bot.change_presence(activity=discord.Game(name=f"use g! to use my command"))
 
-
+@bot.command(name = "hi",aliases =['hello'])
+async def _hi(ctx):
+    await ctx.send(f"Hi {ctx.author.mention}!")
 
 
 bot.run(secret_file['token'])
