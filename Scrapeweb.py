c7bc7cf2423a63cf733b1fa8aa4ce7c22f1ad5e1
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2021-01-31 09:28:16 +0700

    working on the character scraping

--- Scrapeweb.py
+++ Scrapeweb.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import re
-
+def toText(isi):
+    return isi.get_text()
 fakeheader ={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 Searchbar1 = "https://www.gensh.in/database?tx_awsm_database_gidbsearch%5B__referrer%5D%5B%40extension%5D=AwsmDatabase&tx_awsm_database_gidbsearch%5B__referrer%5D%5B%40vendor%5D=AWSM&tx_awsm_database_gidbsearch%5B__referrer%5D%5B%40controller%5D=Search&tx_awsm_database_gidbsearch%5B__referrer%5D%5B%40action%5D=quicksearch&tx_awsm_database_gidbsearch%5B__referrer%5D%5Barguments%5D=YTowOnt9ab12f14b058d7f0afc3ee521eecd856b82073eec&tx_awsm_database_gidbsearch%5B__referrer%5D%5B%40request%5D=a%3A4%3A%7Bs%3A10%3A%22%40extension%22%3Bs%3A12%3A%22AwsmDatabase%22%3Bs%3A11%3A%22%40controller%22%3Bs%3A6%3A%22Search%22%3Bs%3A7%3A%22%40action%22%3Bs%3A11%3A%22quicksearch%22%3Bs%3A7%3A%22%40vendor%22%3Bs%3A4%3A%22AWSM%22%3B%7D0a680789a8fd4255931546477c50d2e2f3c4f6ae&tx_awsm_database_gidbsearch%5B__trustedProperties%5D=a%3A2%3A%7Bs%3A10%3A%22searchterm%22%3Bi%3A1%3Bs%3A6%3A%22filter%22%3Ba%3A1%3A%7Bs%3A11%3A%22quicksearch%22%3Bi%3A1%3B%7D%7D01a0a65640549685085443cca595904d6416f72e&tx_awsm_database_gidbsearch%5Bsearchterm%5D="
 Searchbar2 = "&tx_awsm_database_gidbsearch%5Bfilter%5D%5Bquicksearch%5D=true"
@@ -82,40 +83,54 @@ def Scrape(query):
     addtodictLINKS(refinedlinks)
     addtodictINFO(cards)
     addtodictIMG(refinedimages)
-isian = "/database/consumable/adeptus-temptation"
+isian = "/characters/amber"
 datatest = "/"
+formatres = ""
+
+CHARACTERINFO = {'Name': '','MainIMG':''
+                                        }
+
+
 def Articlescrape(isian):
 
     search = requests.get(linktodatabase+isian).text
     soup = BeautifulSoup(search,'html.parser')
     contentclasifier = isian.split("/")
     db = "database"
-    if contentclasifier[0] == db :
-        if contentclasifier[1]=="npc":
+    if contentclasifier[1] == db :
+        if contentclasifier[2]=="npc":
             pass
-        if contentclasifier[1]=="quest":
+        if contentclasifier[2]=="quest":
             pass
-        if contentclasifier[1]=="artifact-set":
+        if contentclasifier[2]=="artifact-set":
             pass
-        if contentclasifier[1]=="enemies":
+        if contentclasifier[2]=="enemies":
             pass
-        if contentclasifier[1]=="consumable":
+        if contentclasifier[2]=="consumable":
             pass
-        if contentclasifier[1]=="domain":
+        if contentclasifier[2]=="domain":
             pass
-        if contentclasifier[1]=="book":
+        if contentclasifier[2]=="book":
             pass
-        if contentclasifier[1]=="artifact":
+        if contentclasifier[2]=="artifact":
             pass
-        if contentclasifier[1]=="item":
+        if contentclasifier[2]=="item":
             pass
-        if contentclasifier[1]=="weapon":
+        if contentclasifier[2]=="weapon":
             pass
-    for i in contentclasifier:
-        print(i)
-    # characterDetailContainer = soup.find("div",attrs={"class": "character-detail"})
-    # characterDetails = soup.find_all()
-    # print(characterDetailContainer.prettify())
+    if contentclasifier[1] == "characters" :
+        characterDetailContainer = soup.find("div",attrs={"class": "character-detail"})
+        characterDetails = characterDetailContainer.find_all("div",attrs={"class": "row mb-4"})
+        
+        name = characterDetails[0].find("h2")
+        mainImg = characterDetails[0].find("img",attrs ={"class":"img-fluid"})
+        CHARACTERINFO["Name"]= toText(name)
+        CHARACTERINFO["MainIMG"] = mainImg["src"]
+
+    if contentclasifier[1] == "mechanics" :
+        pass
+    
+    
 
 
 
@@ -127,3 +142,4 @@ def Articlescrape(isian):
 # print(INFO["link1"])
 
 Articlescrape(isian)
+print(CHARACTERINFO["Name"],"\n",CHARACTERINFO["MainIMG"])
